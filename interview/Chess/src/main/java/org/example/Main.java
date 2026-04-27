04e359bb8352f45705bc5ef3778f88b59e4b4c2c
Aman Kumar <aman.q.kumar@oracle.com>
2026-04-27 19:55:38 +0530

    init Chess game

---
+++ interview/Chess/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Chess Game");
+        /**
+         * Chess requirements:-
+         * Game
+         * 8x8 board
+         * pieces: king, queen, pawn, rook, bishop, horse
+         * colours: black and white --> one to each player --> 2 players
+         * Player --> move strategy, factory for making
+         * checkWin(), checkDraw() --> board
+         * piece transformation from pawn to any other piece type
+         * piece --> move strategy and factory
+         * move validation for evey piece
+         */
+    }
+}
